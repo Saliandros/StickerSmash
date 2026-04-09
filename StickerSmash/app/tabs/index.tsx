c1efc4ea7e230dc36e88ca1ba4a3adca955d8e25
Saliandros <243761716+Saliandros@users.noreply.github.com>
2026-04-09 12:23:50 +0200

    Add initial project configuration with package.json and tsconfig.json
    - Created package.json with dependencies and scripts for the StickerSmash project.
    - Added tsconfig.json extending from expo's base configuration with strict type checking and path aliases.

---
+++ StickerSmash/app/tabs/index.tsx
@@ -0,0 +1,28 @@
+import { Text, View,  StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link  href="/about" style={styles.button}>Lækker Knap</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#6f42c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
